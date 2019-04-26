3a9ac14d95b5939598df109b5969699e45914b0d
Karen Torres <ketorres10@utpl.edu.ec>
2019-04-26 13:03:55 -0500

    Agregando una BD

--- mybd.sql
+++ mybd.sql
@@ -1,20 +1,22 @@
 -- phpMyAdmin SQL Dump
--- version 4.2.7.1
--- http://www.phpmyadmin.net
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 26-04-2019 a las 19:12:52
--- Versión del servidor: 5.6.20
--- Versión de PHP: 5.5.15
+-- Tiempo de generación: 26-04-2019 a las 20:01:37
+-- Versión del servidor: 10.1.29-MariaDB
+-- Versión de PHP: 7.2.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Base de datos: `mybd`
@@ -26,23 +28,24 @@ SET time_zone = "+00:00";
 -- Estructura de tabla para la tabla `usuario`
 --
 
-CREATE TABLE IF NOT EXISTS `usuario` (
-`id_usuario` int(3) NOT NULL,
+CREATE TABLE `usuario` (
+  `id_usuario` int(3) NOT NULL,
   `cedula` varchar(10) NOT NULL,
   `nombres` varchar(50) NOT NULL,
   `apellidos` varchar(50) NOT NULL,
   `usuario` varchar(20) NOT NULL,
   `contrasenia` varchar(20) NOT NULL,
   `telefono` varchar(10) NOT NULL,
-  `direccion` varchar(100) NOT NULL
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+  `direccion` varchar(100) NOT NULL,
+  `rol` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Volcado de datos para la tabla `usuario`
 --
 
-INSERT INTO `usuario` (`id_usuario`, `cedula`, `nombres`, `apellidos`, `usuario`, `contrasenia`, `telefono`, `direccion`) VALUES
-(1, '1900751015', 'Erika Tatiana', 'Vasquez Tapia', 'evasquez', 'admin', '0979596397', 'Jipiro');
+INSERT INTO `usuario` (`id_usuario`, `cedula`, `nombres`, `apellidos`, `usuario`, `contrasenia`, `telefono`, `direccion`, `rol`) VALUES
+(1, '1900751015', 'Erika Tatiana', 'Vasquez Tapia', 'evasquez', 'admin', '0979596397', 'Jipiro', 'Administrador');
 
 --
 -- Índices para tablas volcadas
@@ -52,7 +55,8 @@ INSERT INTO `usuario` (`id_usuario`, `cedula`, `nombres`, `apellidos`, `usuario`
 -- Indices de la tabla `usuario`
 --
 ALTER TABLE `usuario`
- ADD PRIMARY KEY (`id_usuario`), ADD UNIQUE KEY `cedula` (`cedula`,`usuario`);
+  ADD PRIMARY KEY (`id_usuario`),
+  ADD UNIQUE KEY `cedula` (`cedula`,`usuario`);
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
@@ -62,7 +66,9 @@ ALTER TABLE `usuario`
 -- AUTO_INCREMENT de la tabla `usuario`
 --
 ALTER TABLE `usuario`
-MODIFY `id_usuario` int(3) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+  MODIFY `id_usuario` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
